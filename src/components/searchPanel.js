daf5d51263b9dda276bc4410cbf19a0d5d722447
Kateryna Dorodko <katerynadorodko@Katerynas-MacBook-Pro.local>
2021-10-16 11:27:26 +0300

    separate components into diffent folders and change two func component to class

--- src/components/searchPanel.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const SearchPanel = () => {
-  const searchTag = "Type here to search";
-  return <input placeholder={searchTag} />;
-};
-
-export default SearchPanel;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import AppHeader from "./components/appHeader";
-import SearchPanel from "./components/searchPanel";
-import TodoList from "./components/todoList";
-
-const App = () => {
-  const isLoggedIn = true;
-  const loginBox = <span>Log in please</span>;
-  const welcomeBox = <span>Welcome Back</span>;
-  return (
-    <div>
-      {isLoggedIn ? welcomeBox : loginBox}
-      <AppHeader />
-      <SearchPanel />
-      <TodoList />
-    </div>
-  );
-};
+import App from "./components/app";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
